fa21768afbc71da910ba823306ab860f64e226fa
Benny Dreamly <bgaming0277@gmail.com>
2025-01-24 14:56:53 -0700

    Normalize line endings

--- worlds/ror2/Options.py
+++ worlds/ror2/Options.py
@@ -1,10 +1,10 @@
-from typing import Dict
-from Options import Option, Toggle, DefaultOnToggle, DeathLink, Range, Choice
+from dataclasses import dataclass
+from Options import Toggle, DefaultOnToggle, DeathLink, Range, Choice, PerGameCommonOptions, OptionGroup
 
 
 # NOTE be aware that since the range of item ids that RoR2 uses is based off of the maximums of checks
 # Be careful when changing the range_end values not to go into another game's IDs
-# NOTE that these changes to range_end must also be reflected in the RoR2 client so it understands the same ids.
+# NOTE that these changes to range_end must also be reflected in the RoR2 client, so it understands the same ids.
 
 class Goal(Choice):
     """
@@ -16,6 +16,21 @@ class Goal(Choice):
     display_name = "Game Mode"
     option_classic = 0
     option_explore = 1
+    default = 1
+
+
+class Victory(Choice):
+    """
+    Mithrix: Defeat Mithrix in Commencement
+    Voidling: Defeat the Voidling in The Planetarium (DLC required! Will select any if not enabled.)
+    Limbo: Defeat the Scavenger in Hidden Realm: A Moment, Whole
+    Any: Any victory in the game will count. See Final Stage Death for additional ways.
+    """
+    display_name = "Victory Condition"
+    option_any = 0
+    option_mithrix = 1
+    option_voidling = 2
+    option_limbo = 3
     default = 0
 
 
@@ -48,7 +63,8 @@ class ScavengersPerEnvironment(Range):
     display_name = "Scavenger per Environment"
     range_start = 0
     range_end = 1
-    default = 1
+    default = 0
+
 
 class ScannersPerEnvironment(Range):
     """Explore Mode: The number of scanners locations per environment."""
@@ -57,6 +73,7 @@ class ScannersPerEnvironment(Range):
     range_end = 1
     default = 1
 
+
 class AltarsPerEnvironment(Range):
     """Explore Mode: The number of altars locations per environment."""
     display_name = "Newts Per Environment"
@@ -64,9 +81,10 @@ class AltarsPerEnvironment(Range):
     range_end = 2
     default = 1
 
+
 class TotalRevivals(Range):
     """Total Percentage of `Dio's Best Friend` item put in the item pool."""
-    display_name = "Total Revives"
+    display_name = "Total Revives as percentage"
     range_start = 0
     range_end = 10
     default = 4
@@ -83,6 +101,7 @@ class ItemPickupStep(Range):
     range_end = 5
     default = 1
 
+
 class ShrineUseStep(Range):
     """
     Explore Mode:
@@ -96,6 +115,11 @@ class ShrineUseStep(Range):
     default = 0
 
 
+class AllowTrapItems(Toggle):
+    """Allows Trap items in the item pool."""
+    display_name = "Enable Trap Items"
+
+
 class AllowLunarItems(DefaultOnToggle):
     """Allows Lunar items in the item pool."""
     display_name = "Enable Lunar Item Shuffling"
@@ -107,16 +131,15 @@ class StartWithRevive(DefaultOnToggle):
 
 
 class FinalStageDeath(Toggle):
-    """Death on the final boss stage counts as a win."""
+    """The following will count as a win if set to "true", and victory is set to "any":
+    Dying in Commencement.
+    Dying in The Planetarium.
+    Obliterating yourself
+    If not use the following to tell if final stage death will count:
+    Victory: mithrix - only dying in Commencement will count.
+    Victory: voidling - only dying in The Planetarium will count.
+    Victory: limbo - Obliterating yourself will count."""
     display_name = "Final Stage Death is Win"
-
-
-class BeginWithLoop(Toggle):
-    """
-    Enable to precollect a full loop of environments.
-    Only has an effect with Explore Mode.
-    """
-    display_name = "Begin With Loop"
 
 
 class DLC_SOTV(Toggle):
@@ -127,6 +150,16 @@ class DLC_SOTV(Toggle):
      """
     display_name = "Enable DLC - SOTV"
 
+
+class RequireStages(DefaultOnToggle):
+    """Add Stage items to the pool to block access to the next set of environments."""
+    display_name = "Require Stages"
+
+
+class ProgressiveStages(DefaultOnToggle):
+    """This will convert Stage items to be a progressive item. For example instead of "Stage 2" it would be
+     "Progressive Stage" """
+    display_name = "Progressive Stages"
 
 
 class GreenScrap(Range):
@@ -241,69 +274,184 @@ class Equipment(Range):
     default = 32
 
 
+class Money(Range):
+    """Weight of money items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Money"
+    range_start = 0
+    range_end = 100
+    default = 64
+
+
+class LunarCoin(Range):
+    """Weight of lunar coin items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Lunar Coins"
+    range_start = 0
+    range_end = 100
+    default = 20
+
+
+class Experience(Range):
+    """Weight of 1000 exp items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "1000 Exp"
+    range_start = 0
+    range_end = 100
+    default = 40
+
+
+class MountainTrap(Range):
+    """Weight of mountain trap items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Mountain Trap"
+    range_start = 0
+    range_end = 100
+    default = 5
+
+
+class TimeWarpTrap(Range):
+    """Weight of time warp trap items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Time Warp Trap"
+    range_start = 0
+    range_end = 100
+    default = 20
+
+
+class CombatTrap(Range):
+    """Weight of combat trap items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Combat Trap"
+    range_start = 0
+    range_end = 100
+    default = 20
+
+
+class TeleportTrap(Range):
+    """Weight of teleport trap items in the item pool.
+
+    (Ignored unless Item Weight Presets is 'No')"""
+    display_name = "Teleport Trap"
+    range_start = 0
+    range_end = 100
+    default = 20
+
+
 class ItemPoolPresetToggle(Toggle):
     """Will use the item weight presets when set to true, otherwise will use the custom set item pool weights."""
     display_name = "Use Item Weight Presets"
 
 
 class ItemWeights(Choice):
-    """Set item_pool_presets to true if you want to use one of these presets.
+    """Set Use Item Weight Presets to yes if you want to use one of these presets.
     Preset choices for determining the weights of the item pool.
     - New is a test for a potential adjustment to the default weights.
     - Uncommon puts a large number of uncommon items in the pool.
     - Legendary puts a large number of legendary items in the pool.
-    - Lunartic makes everything a lunar item.
-    - Chaos generates the pool completely at random with rarer items having a slight cap to prevent this option being too easy.
+    - Chaos generates the pool completely at random with rarer items having a slight cap to prevent this option being
+    too easy.
     - No Scraps removes all scrap items from the item pool.
     - Even generates the item pool with every item having an even weight.
     - Scraps Only will be only scrap items in the item pool.
+    - Lunartic makes everything a lunar item.
     - Void makes everything a void item."""
     display_name = "Item Weights"
     option_default = 0
     option_new = 1
     option_uncommon = 2
     option_legendary = 3
-    option_lunartic = 4
-    option_chaos = 5
-    option_no_scraps = 6
-    option_even = 7
-    option_scraps_only = 8
+    option_chaos = 4
+    option_no_scraps = 5
+    option_even = 6
+    option_scraps_only = 7
+    option_lunartic = 8
     option_void = 9
 
 
-# define a dictionary for the weights of the generated item pool.
-ror2_weights: Dict[str, type(Option)] = {
-    "green_scrap":          GreenScrap,
-    "red_scrap":            RedScrap,
-    "yellow_scrap":         YellowScrap,
-    "white_scrap":          WhiteScrap,
-    "common_item":          CommonItem,
-    "uncommon_item":        UncommonItem,
-    "legendary_item":       LegendaryItem,
-    "boss_item":            BossItem,
-    "lunar_item":           LunarItem,
-    "void_item":            VoidItem,
-    "equipment":            Equipment
-}
+ror2_option_groups = [
+    OptionGroup("Explore Mode Options", [
+        ChestsPerEnvironment,
+        ShrinesPerEnvironment,
+        ScavengersPerEnvironment,
+        ScannersPerEnvironment,
+        AltarsPerEnvironment,
+        RequireStages,
+        ProgressiveStages,
+    ]),
+    OptionGroup("Classic Mode Options", [
+        TotalLocations,
+    ], start_collapsed=True),
+    OptionGroup("Weighted Choices", [
+        ItemWeights,
+        ItemPoolPresetToggle,
+        WhiteScrap,
+        GreenScrap,
+        YellowScrap,
+        RedScrap,
+        CommonItem,
+        UncommonItem,
+        LegendaryItem,
+        BossItem,
+        LunarItem,
+        VoidItem,
+        Equipment,
+        Money,
+        LunarCoin,
+        Experience,
+        MountainTrap,
+        TimeWarpTrap,
+        CombatTrap,
+        TeleportTrap,
+    ]),
+]
 
-ror2_options: Dict[str, type(Option)] = {
-    "goal":                     Goal,
-    "total_locations":          TotalLocations,
-    "chests_per_stage":         ChestsPerEnvironment,
-    "shrines_per_stage":        ShrinesPerEnvironment,
-    "scavengers_per_stage":     ScavengersPerEnvironment,
-    "scanner_per_stage":        ScannersPerEnvironment,
-    "altars_per_stage":         AltarsPerEnvironment,
-    "total_revivals":           TotalRevivals,
-    "start_with_revive":        StartWithRevive,
-    "final_stage_death":        FinalStageDeath,
-    "begin_with_loop":          BeginWithLoop,
-    "dlc_sotv":                 DLC_SOTV,
-    "death_link":               DeathLink,
-    "item_pickup_step":         ItemPickupStep,
-    "shrine_use_step":          ShrineUseStep,
-    "enable_lunar":             AllowLunarItems,
-    "item_weights":             ItemWeights,
-    "item_pool_presets":        ItemPoolPresetToggle,
-    **ror2_weights
-}
+
+@dataclass
+class ROR2Options(PerGameCommonOptions):
+    goal: Goal
+    victory: Victory
+    total_locations: TotalLocations
+    chests_per_stage: ChestsPerEnvironment
+    shrines_per_stage: ShrinesPerEnvironment
+    scavengers_per_stage: ScavengersPerEnvironment
+    scanner_per_stage: ScannersPerEnvironment
+    altars_per_stage: AltarsPerEnvironment
+    total_revivals: TotalRevivals
+    start_with_revive: StartWithRevive
+    final_stage_death: FinalStageDeath
+    dlc_sotv: DLC_SOTV
+    require_stages: RequireStages
+    progressive_stages: ProgressiveStages
+    death_link: DeathLink
+    item_pickup_step: ItemPickupStep
+    shrine_use_step: ShrineUseStep
+    enable_trap: AllowTrapItems
+    enable_lunar: AllowLunarItems
+    item_weights: ItemWeights
+    item_pool_presets: ItemPoolPresetToggle
+    # define the weights of the generated item pool.
+    white_scrap: WhiteScrap
+    green_scrap: GreenScrap
+    yellow_scrap: YellowScrap
+    red_scrap: RedScrap
+    common_item: CommonItem
+    uncommon_item: UncommonItem
+    legendary_item: LegendaryItem
+    boss_item: BossItem
+    lunar_item: LunarItem
+    void_item: VoidItem
+    equipment: Equipment
+    money: Money
+    lunar_coin: LunarCoin
+    experience: Experience
+    mountain_trap: MountainTrap
+    time_warp_trap: TimeWarpTrap
+    combat_trap: CombatTrap
+    teleport_trap: TeleportTrap
